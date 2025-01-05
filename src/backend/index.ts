66a68058d04bb9e584d10cc4efddf50ddd39e293
Aditya Bhargava <adit@adit.io>
2025-01-04 19:14:39 -0600

    cors

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -2,12 +2,28 @@ import path from "path";
 import express from "express";
 import { fileURLToPath } from "url";
 import { add } from "../common/util.js";
+import cors from "cors";
+
 const app = express();
 const port = 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(express.static("dist/frontend"));
+app.use(cors({ origin: "http://localhost:3000" }));
+
+app.use((req, res, next) => {
+  next();
+  console.log(`${req.method} ${req.url} -> ${res.statusCode}`);
+});
+
+app.use(express.json({ limit: "5mb" }));
+app.use(
+  express.urlencoded({
+    limit: "5mb",
+    extended: true,
+  })
+);
 
 app.get("/hi", (req, res) => {
   res.send(`Hello World! ${add(1, 2)}`);
