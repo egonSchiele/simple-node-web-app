6eb95863b702ac739a4f1f763629e4954fabe49b
Aditya Bhargava <adit@adit.io>
2025-02-02 11:50:20 -0600

    sass + css modules

--- src/frontend/Index.tsx
+++ src/frontend/Index.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import "./globals.scss";
+import classes from "./index.module.scss";
+import { cls } from "../common/util.js";
+
 const App = () => {
-  return <h1>Hello World from React!</h1>;
+  return <h1 className={cls(classes.header)}>Hello World from React!</h1>;
 };
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
