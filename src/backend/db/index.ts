be252d9b1649e1c00933cd555ba7fb82b2f722f8
Aditya Bhargava <adit@adit.io>
2025-02-07 17:06:07 -0600

    add zod, get db info from env

--- src/backend/db/index.ts
+++ src/backend/db/index.ts
@@ -1,14 +1,15 @@
 import { Database } from "./types.js"; // this is the Database interface we defined earlier
 import pg from "pg";
 import { Kysely, PostgresDialect } from "kysely";
+import config from "@/backend/lib/config.js";
 const { Pool } = pg;
 const dialect = new PostgresDialect({
   pool: new Pool({
-    host: "localhost",
-    port: 5432,
-    user: "postgres",
-    password: "password",
-    database: "your-database-name",
+    host: config.DB_HOST,
+    port: config.DB_PORT,
+    user: config.DB_USER,
+    password: config.DB_PASSWORD,
+    database: config.DB_DATABASE,
     ssl: {
       // You will probably want to use SSL in prod
       // But this disables it for now so you can test
--- src/backend/lib/config.ts
+++ src/backend/lib/config.ts
@@ -1,3 +1,16 @@
 import path from "path";
-const __dirname = import.meta.dirname;
+import { z } from "zod";
+export const __dirname = import.meta.dirname;
 export const ROOTDIR = path.join(__dirname, "../../");
+
+const configSchema = z.object({
+  DB_HOST: z.string().default("localhost"),
+  DB_PORT: z.number().default(5432),
+  DB_USER: z.string().default("postgres"),
+  DB_PASSWORD: z.string().default("password"),
+  DB_DATABASE: z.string().default("your-database-name"),
+});
+
+type Config = z.infer<typeof configSchema>;
+const config: Config = configSchema.parse(process.env);
+export default config;
