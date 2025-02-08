0fa0538839fca436c80e70e644ec52289142382a
Aditya Bhargava <adit@adit.io>
2025-02-07 22:22:06 -0600

    aliases, rm about

--- src/backend/lib/router/entrypoints.ts
+++ src/backend/lib/router/entrypoints.ts
@@ -4,10 +4,22 @@ import path from "path";
 
 const entrypointsRouter = Router();
 
-const entrypoints = ["index.html", "about.html", "signin.html", "signup.html"];
+const entrypoints = ["index.html", "signin.html", "signup.html"];
 
 entrypoints.forEach((entrypoint) => {
   entrypointsRouter.get(`/${entrypoint}`, (req, res) =>
+    res.sendFile(path.join(PAGESDIR, entrypoint))
+  );
+});
+
+const aliases = {
+  "/": "index.html",
+  "/signin": "signin.html",
+  "/signup": "signup.html",
+};
+
+Object.entries(aliases).forEach(([alias, entrypoint]) => {
+  entrypointsRouter.get(alias, (req, res) =>
     res.sendFile(path.join(PAGESDIR, entrypoint))
   );
 });
--- src/frontend/pages/About.tsx
+++
@@ -1,9 +0,0 @@
-import "./globals.css";
-import React from "react";
-import { createRoot } from "react-dom/client";
-const App = () => {
-  return <h1>About me</h1>;
-};
-
-const root = createRoot(document.getElementById("root") as HTMLElement);
-root.render(<App />);
