554332a6c0915ef61c7cc5c092441494d5e648a8
Aditya Bhargava <adit@adit.io>
2025-06-15 13:52:58 -0500

    vite should find all html files

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,22 @@ import path from "path";
 import { fileURLToPath } from "url";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-
+import fs from "fs";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+const pagesDir = path.resolve(__dirname, "pages");
+const htmlFiles = fs
+  .readdirSync(pagesDir)
+  .filter((file) => file.endsWith(".html"))
+  .map((file) => path.resolve(pagesDir, file));
+
+console.log("HTML files found:", htmlFiles);
+const input = htmlFiles.reduce((acc, file) => {
+  const name = path.basename(file, ".html");
+  acc[name] = file;
+  return acc;
+}, {});
 
 export default defineConfig({
   plugins: [tailwindcss(), react()],
@@ -22,11 +35,7 @@ export default defineConfig({
   build: {
     outDir: "dist/frontend",
     rollupOptions: {
-      input: {
-        index: path.resolve(__dirname, "pages", "index.html"),
-        signin: path.resolve(__dirname, "pages", "signin.html"),
-        signup: path.resolve(__dirname, "pages", "signup.html"),
-      },
+      input,
     },
   },
 });
